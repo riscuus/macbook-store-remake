80c9d78e68395e42a1d74e1c0fb39616e7e4dad2
riscuus <riscoalbert@gmail.com>
2017-11-10 16:12:51 +0100

    1st commit

---
+++ js/animations.js
@@ -0,0 +1,99 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.concepto-caption',{
+  delay:2000,
+  duration: 3000,
+  origin: 'bottom',
+  scale: 1,
+  distance: '60px',
+  easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+
+});
+
+sr.reveal('.video',{
+  delay:1900,
+  duration: 3000,
+  origin: 'bottom',
+  scale: 1,
+  distance: '40px',
+  easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+
+});
+
+sr.reveal('#concepto',{
+  delay: 500,
+  distance: '30px',
+  duration: 4000,
+  origin: 'top',
+  scale: 1,
+  easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+
+});
+
+sr.reveal('#navbar',{
+  delay:4500,
+  duration: 1500,
+  distance:0,
+  scale: 1,
+  easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+
+});
+
+sr.reveal('.num1',{
+  delay:0,
+  duration: 800,
+  distance: '40px',
+  origin: 'right',
+  scale: 1,
+  easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+
+});
+
+sr.reveal('.num2',{
+  delay:150,
+  duration: 800,
+  distance: '40px',
+  origin: 'right',
+  scale: 1,
+  easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+
+});
+sr.reveal('.num3',{
+  delay:300,
+  duration: 800,
+  distance: '40px',
+  origin: 'right',
+  scale: 1,
+  easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+
+});
+sr.reveal('.num4',{
+  delay:450,
+  duration: 800,
+  distance: '40px',
+  origin: 'right',
+  scale: 1,
+  easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+
+});
+
+
+sr.reveal('.cable', {
+  duration: 2000,
+  origin: 'left',
+  scale: 1,
+  distance: '200px',
+  easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+  opacity:1,
+  viewFactor: 0.4
+});
+
+sr.reveal('.mac-no-cable', {
+  duration: 2000,
+  origin: 'right',
+  scale: 1,
+  distance: '200px',
+  easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+  opacity:1,
+  viewFactor: 0.4
+});
